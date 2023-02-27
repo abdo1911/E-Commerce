f7137c38aaca0295a212294ba93c937fda6491ce
Abd el rahman yasser <108436892+abdo1911@users.noreply.github.com>
2023-02-27 21:33:32 +0200

    update

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -13,19 +13,19 @@
         @csrf
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Name</label>
-            <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input type="text" value="{{$product->name}}" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Price</label>
-            <input type="text" name="price" class="form-control" id="exampleInputPassword1">
+            <input type="text" name="price" value="{{$product->price}}" class="form-control" id="exampleInputPassword1">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Description</label>
-            <input type="text" name="description" class="form-control" id="exampleInputPassword1">
+            <input type="text" name="description" value="{{$product->description}}" class="form-control" id="exampleInputPassword1">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">mobile</label>
-            <input type="text" name="mobile" class="form-control" id="exampleInputPassword1">
+            <input type="text" name="mobile" value="{{$product->mobile}}" class="form-control" id="exampleInputPassword1">
         </div>
         <label for="exampleInputPassword1" class="form-label">Type</label>
         <select name = "category_id" class="form-select" aria-label="Default select example">
@@ -38,13 +38,8 @@
             <label for="exampleInputEmail1" class="form-label">Logo</label>
             <input type='file' name="logo" class="form-control" placeholder="Enter Email...">
         </div>
-
         <br>
         <button type="submit" class="btn btn-primary">Update</button>
-
-
     </form>
-
-
 
 @endsection
